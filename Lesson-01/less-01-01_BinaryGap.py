8f564ff3f0b3260d57bbc9674a24d5ed7e210ce2
JavadZahrabi <ja.zahrabi@gmail.com>
2022-07-02 15:18:12 +0200

    add input and raise

--- Lesson-01/less-01-01_BinaryGap.py
+++ Lesson-01/less-01-01_BinaryGap.py
@@ -1,1 +1,24 @@
-print ("Hello world")
+
+from tkinter import N
+
+
+
+
+MAXINT = 2147483647
+
+def solution(N):
+    
+    if not isinstance(N, int):
+        raise TypeError("Input must be an Integer")
+    
+    if N < 1:
+        raise ValueError("Input must be a positive Integer")
+
+    if N > MAXINT:
+        raise ValueError("Input must be a positive integer less than 2,147,483,647")
+    
+    binary_string = str(bin(N))[2:]
+
+
+N = input ("Enter a positive integer number:")
+solution(N)
